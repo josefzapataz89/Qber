c44e4058b430a8bd0e724d506728780407e377c0
José Fernando Zapata Zambrano <joferzz1989@gmail.com>
2015-03-04 17:38:57 -0430

    Corrección de código
    reparación del controlador, el modal de recuperación de información ya
    se despliega. Pantalla aun no esta funcional.

--- chat/www/js/controllers.js
+++ chat/www/js/controllers.js
@@ -1,22 +1,35 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic'])
 
-.controller('inicioCtrl', function($scope, $ionicModal, $timeout){
-  $scope.userData = {};
+.controller('InicioCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the login modal
+  $scope.dataUsuario = {};
+
+  // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/recuperar.html', {
-    scope: $scope  
-  }).then(function(modal){
-      $scope.modal = modal;
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
   });
-  $scope.cerrarRecuperar = function(){
+
+  // Triggered in the login modal to close it
+  $scope.cerrarRecuperar = function() {
     $scope.modal.hide();
   };
-  $scope.abrirRecuperar = function(){
+
+  // Open the login modal
+  $scope.abrirRecuperar = function() {
     $scope.modal.show();
   };
-  $scope.recuperar = function(){
-    console.log('recuperar contrasena', $scope.userData);
+
+  // Perform the login action when the user submits the login form
+  $scope.recuperar = function() {
+    console.log('recuperando', $scope.dataUsuario);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
     $timeout(function() {
-      $scope.cerrarRecuperar();
+      $scope.closeLogin();
     }, 1000);
   };
 })
