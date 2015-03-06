ef4bd1b4b7781a124cb84254f26bca2ca18ff9b5
José Fernando Zapata Zambrano <joferzz1989@gmail.com>
2015-03-06 17:55:24 -0430

    Reestructuración de la aplicación
    la plantilla anterior presentaba bugs, por lo que se decidió cambiar la
    plantilla.

--- chat/www/js/app.js
+++ chat/www/js/app.js
@@ -1,5 +1,13 @@
-angular.module('starter', ['ionic', 'starter.controllers'])
-.run( function($ionicPlatform) {
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -12,21 +20,74 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
 })
-.config( function($stateProvider, $urlRouterProvider) {
-  $stateProvider.state('app', {
-    url: "/app",
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: "/tab",
     abstract: true,
-    templateUrl: "templates/inicio.html",
-    controller: 'InicioCtrl'
+    templateUrl: "templates/tabs.html"
   })
-  .state('app.search', {
-    url: "/search",
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.Contactos', {
+    url: '/Contactos',
     views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
+      'tab-Contactos': {
+        templateUrl: 'templates/tab-Contactos.html',
+        controller: 'ContactsCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.perfil', {
+    url: '/perfil',
+    views: {
+      'tab-perfil': {
+        templateUrl: 'templates/tab-perfil.html',
+        controller: 'perfilCtrl'
+      }
+    }
+  })
+
+  .state('tab.configuracion', {
+    url: '/configuracion',
+    views: {
+      'tab-configuracion': {
+        templateUrl: 'templates/tab-Configuracion.html',
+        controller: 'configuracionCtrl'
       }
     }
   });
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/search');
+  $urlRouterProvider.otherwise('/tab/chats');
+
 });
