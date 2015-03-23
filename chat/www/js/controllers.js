6044dc76c10e4901c01f33fe27ed7876fc35cdf6
jose f zapata z <joferzz1989@gmail.com>
2015-03-23 15:54:57 -0430

    respaldo

--- chat/www/js/controllers.js
+++ chat/www/js/controllers.js
@@ -15,21 +15,21 @@ $scope.nuevo = {};
         $http.get('http://localhost:5000/api/usuarios/'+ $scope.dataInicio.username)
         .success(function(data){
           if(data && data.email== $scope.dataInicio.username && data.contrasena==$scope.dataInicio.password){
-          console.log('data: '+data);
-          USER.nombre=data.nombre;
-          console.log('entroo');
-          USER.correo=data.email;
-          USER.estado=data.estado;
-          USER.id=data.id;   
-          console.log(USER);
-          $state.go('tab.chats');
-                }
-                else{
-        var alertPopup = $ionicPopup.alert({
-        title: 'Error al Iniciar!',
-        template: 'Porfavor Revisa tu usuario o contraseña!'
-        });
-                }
+            console.log('data: '+data);
+            USER.nombre=data.nombre;
+            console.log('entroo');
+            USER.correo=data.email;
+            USER.estado=data.estado;
+            USER.id=data.id;   
+            console.log(USER);
+            $state.go('tab.chats');
+          }
+          else{
+            var alertPopup = $ionicPopup.alert({
+            title: 'Error al Iniciar!',
+            template: 'Porfavor Revisa tu usuario o contraseña!'
+            });
+          }
   
         })
         .error(function(error){
@@ -67,6 +67,8 @@ $scope.nuevo = {};
         })
       .success(function(data){
         console.log(data);
+        $scope.dataUsuario = {};
+        $scope.cerrarModal(1);
       })
       .error(function(err){
         console.log(err);
@@ -238,10 +240,6 @@ $scope.nuevo2 = {};
   $scope.remove = function(chat) { 
     Chats.remove(chat);
   };
-})
-
-.controller('ChatsCtrl', function($scope, Sesion) {
-  
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams) {
